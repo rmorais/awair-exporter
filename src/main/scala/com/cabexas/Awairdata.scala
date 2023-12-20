73ade8faf059164b5a4ef87a700a631a02429737
rmorais <2330276+rmorais@users.noreply.github.com>
2023-12-20 21:37:30 +0000

    Replace Decoder/Encoder witha Codec

--- src/main/scala/com/cabexas/Awairdata.scala
+++ src/main/scala/com/cabexas/Awairdata.scala
@@ -1,7 +1,6 @@
 package com.cabexas
 
-import io.circe.Decoder
-import io.circe.Encoder
+import io.circe.Codec
 
 final case class AwairData(
     timestamp: String,
@@ -13,37 +12,8 @@ final case class AwairData(
 
 object AwairData {
 
-  implicit val awairDataDecoder: Decoder[AwairData] = {
-    Decoder.forProduct5(
-      "timestamp",
-      "score",
-      "dew_point",
-      "temp",
-      "humid"
-    ) { AwairData.apply }
-  }
-  implicit val awairdataEncoder: Encoder[AwairData] = 
-    Encoder.forProduct5("timestamp",
-      "score",
-      "dew_point",
-      "temp",
-      "humid")(d => (d.timestamp, d.score, d.dewPoint, d.temperature, d.humidity))
+  implicit val awairDataCodec: Codec[AwairData] =
+    Codec.forProduct5("timestamp", "score", "dew_point", "temp", "humid")(
+      AwairData.apply
+    )(d => (d.timestamp, d.score, d.dewPoint, d.temperature, d.humidity))
 }
-
-// {
-//   "timestamp": "2023-12-15T22:28:50.175Z",
-//   "score": 80,
-//   "dew_point": 12.05,
-//   "temp": 19.11,
-//   "humid": 63.61,
-//   "abs_humid": 10.41,
-//   "co2": 926,
-//   "co2_est": 808,
-//   "co2_est_baseline": 36522,
-//   "voc": 333,
-//   "voc_baseline": 38906,
-//   "voc_h2_raw": 25,
-//   "voc_ethanol_raw": 35,
-//   "pm25": 8,
-//   "pm10_est": 9
-// }
