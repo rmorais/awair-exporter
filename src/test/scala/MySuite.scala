50aedd93f1595d1df90014f371a36b56e274280a
rmorais <2330276+rmorais@users.noreply.github.com>
2023-12-20 22:03:27 +0000

    Rename test

--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -15,7 +15,7 @@ import java.time.Instant
 class MySuite extends DisciplineSuite {
   implicit val eqAwairData: Eq[AwairData] = Eq.fromUniversalEquals
   //this test should be replaced by circe-golden
-  test("roundtrip encoding decoding") {
+  test("decode latest data from awair") {
     val input =
       """{"timestamp":"2023-12-15T22:28:50.175Z","score":80,"dew_point":12.05,"temp":19.11,"humid":63.61,"abs_humid":10.41,"co2":926,"co2_est":808,"co2_est_baseline":36522,"voc":333,"voc_baseline":38906,"voc_h2_raw":25,"voc_ethanol_raw":35,"pm25":8,"pm10_est":9}"""
     val expected =
